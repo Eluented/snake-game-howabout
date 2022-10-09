2b4d2c2b13af7998019ff2dff2df50afdcec5908
Onur <onurbelek@outlook.com>
2022-10-09 12:28:22 +0100

    debugged descending order in the leaderboard

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('score');
+            $table->integer('score');
             $table->string('difficulty');
             $table->timestamp('created_at');
         });
